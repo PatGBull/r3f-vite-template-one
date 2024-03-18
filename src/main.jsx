21ccfaf38645c83fcb46d1ab4ac5e0c0fab3f46f
PatGBull <patrickgbull1@gmail.com>
2024-03-18 15:09:07 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
+import { Perf } from 'r3f-perf'
 import Scene from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<StrictMode>
+	  <Perf />
 	  <Scene />
 	</StrictMode>
 )
