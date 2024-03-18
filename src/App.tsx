b5ef8be7c6acf3f2dda4ca053f2b945017405578
PatGBull <patrickgbull1@gmail.com>
2024-03-18 14:31:19 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import { CameraControls, Environment } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import {useMemo, useRef} from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+import {v4 as uuidv4} from 'uuid'
+import {range} from './utils'
+import './scene.css'
 
-function App() {
-	return (
-		<div className="h-full w-full flex flex-col justify-center items-center gap-8">
-			<h1>Welcome to the starter template</h1>
-			<div id="canvas-container" className="h-1/2 w-3/4">
-				<Canvas>
-					<color args={['hsl(10, 60%, 60%)']} attach="background" />
-					<Environment preset="sunset" />
-					<CameraControls makeDefault />
-					<ambientLight intensity={0.1} />
-					<directionalLight color="red" position={[0, 0, 5]} />
-					<mesh>
-						<boxGeometry />
-						<meshStandardMaterial />
-					</mesh>
-				</Canvas>
-			</div>
+const Scene = () => {
+  return (
+      <div>
+	  <h1>Welcome to the starter template</h1>
+		<div id="canvas-container">
+		    <Canvas>
+			<color args={['hsl(10, 60%, 60%)']} attach="background" />
+			<Environment preset="sunset" />
+			<CameraControls makeDefault />
+			<ambientLight intensity={0.1} />
+			<directionalLight color="red" position={[0, 0, 5]} />
+				<mesh>
+				  <boxGeometry />
+				  <meshStandardMaterial />
+				</mesh>
+		     </Canvas>
 		</div>
-	)
-}
+	</div>
+	     )
+	}
 
-export default App
+export default Scene
