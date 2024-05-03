6bc94a92270b850d98ade9d5948493d7b7be2b47
PatGBull <patrickgbull1@gmail.com>
2024-05-03 19:45:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import { CameraControls, Environment } from '@react-three/drei'
-import {useMemo, useRef} from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import {useMemo, useRef, useState } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
 import * as THREE from 'three'
 import {v4 as uuidv4} from 'uuid'
 import {range} from './utils'
 import './scene.css'
 
+import { colors } from "./data";
+import { useStore } from "./store";
+
+function Objects() {
+	
+	const [index, setIndex] = useState(0);
+
+	const play = useStore((s) => s.play);
+
+	 const default = useRef();
+
+  	const {
+    	viewport: { width, height },
+  	} = useThree();
+}
+
 const Scene = () => {
   return (
       <div>
-	  <h1></h1>
+	  <h1>New Entry</h1>
 		<div id="canvas-container">
 		    <Canvas  camera={{ position: [0, 0, 4], fov: 45>
 			<Perf position={top-left} />
