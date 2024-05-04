23fadf4457ef5d8efe355a6becf4423979fca7d8
PatGBull <patrickgbull1@gmail.com>
2024-05-05 00:26:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { CameraControls, Environment, View, Html } from '@react-three/drei'
+import { useIntersect, Image, Scroll, ScrollControls, CameraControls, Environment, View, Html } from '@react-three/drei'
 import { useMemo, useRef, useState } from 'react'
 import { Canvas, useFrame, useThree, addEffect } from '@react-three/fiber'
 import { useSprings, a } from '@react-spring/three'
@@ -27,6 +27,40 @@ function Objects() {
 
   	//const { viewport: { width, height }, } = useThree();
 	
+}
+
+function Item({ url, scale, ...props }) {
+  	const visible = useRef(false)
+  	const [hovered, hover] = useState(false)
+  	const ref = useIntersect((isVisible) => (visible.current = isVisible))
+ 	const { height } = useThree((state) => state.viewport)
+ 	 	useFrame((state, delta) => {
+    			ref.current.position.y = THREE.MathUtils.damp(ref.current.position.y, visible.current ? 0 : -height / 2 + 1, 4, delta)
+    			ref.current.material.zoom = THREE.MathUtils.damp(ref.current.material.zoom, visible.current ? 1 : 1.5, 4, delta)
+    			ref.current.material.grayscale = THREE.MathUtils.damp(ref.current.material.grayscale, hovered ? 1 : 0, 4, delta)
+  			})
+  	return (
+    	<group {...props}>
+      	<Image ref={ref} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} scale={scale} url={url} />
+    	</group>
+  	)
+       }
+
+function Items() {
+  const { width: w, height: h } = useThree((state) => state.viewport)
+  return (
+    <Scroll>
+      <Item url="/1.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 6, 0, 0]} />
+{/*       <Item url="/2.jpg" scale={[2, w / 3, 1]} position={[w / 30, -h, 0]} />
+      <Item url="/3.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 1, 0]} />
+      <Item url="/4.jpg" scale={[w / 5, w / 5, 1]} position={[w / 4, -h * 1.2, 0]} />
+      <Item url="/5.jpg" scale={[w / 5, w / 5, 1]} position={[w / 10, -h * 1.75, 0]} />
+      <Item url="/6.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 4, -h * 2, 0]} />
+      <Item url="/7.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 2.6, 0]} />
+      <Item url="/8.jpg" scale={[w / 2, w / 2, 1]} position={[w / 4, -h * 3.1, 0]} />
+      <Item url="/12.jpg" scale={[w / 2.5, w / 2, 1]} position={[-w / 6, -h * 4.1, 0]} /> */}
+    </Scroll>
+  )
 }
 
 function Box({ text, color, ...props }) {
@@ -77,6 +111,7 @@ export default function Scene() {
         	   eventPrefix="offset">
 		<CameraControls makeDefault />
 		<Perf position={top-left} />
+		<ScrollControls damping={6} pages={5}>
 			
 		<color args={['hsl(10, 60%, 60%)']} attach="background" />
 		<Environment preset="sunset" />
@@ -90,6 +125,8 @@ export default function Scene() {
 				<meshStandardMaterial />
 				</mesh>
 		 	</ScrollContainer>
+			<Items />
+		</ScrollControls>
 		</Canvas>
 			<div
         		ref={scrollRef}
