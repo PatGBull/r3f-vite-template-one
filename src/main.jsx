572f4b6a223e71adba8714d6a85187e864b96e05
PatGBull <patrickgbull1@gmail.com>
2024-05-04 17:58:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
       <a href="https://pmnd.rs/" style={{ position: 'absolute', top: 40, left: 90, fontSize: '13px' }}>
-        pmnd.rs
+        Title
         <br />
-        dev collective
+       For web Page
       </a>
-      <div style={{ position: 'absolute', top: 40, right: 40, fontSize: '13px' }}>10/17/2021</div>
+      <div style={{ position: 'absolute', top: 40, right: 40, fontSize: '13px' }}>What's the date again?</div>
     </div>
   )
 }
